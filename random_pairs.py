619775b43c981932ce773343cea34fd1f18deb3e
Carole Lavillonnière <carole-lavillonniere@users.noreply.github.com>
2021-10-20 15:51:29 +0200

    Create random_pairs.py

---
+++ random_pairs.py
@@ -0,0 +1,3 @@
+student_names = ['Ayat','Angelianna','Camila','Christina','Dilara','Fernanda','Kemisola','Nailya','Patricia','Roaa','Rowena','Sabrine','Shahrzad','Walaa']
+
+paired_student_names = ['Patricia','Rowena']
